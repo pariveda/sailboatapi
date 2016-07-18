98549d744db44f89b31474c43a6f9c9ebc038211
Dan Hogue <chmod740@gmail.com>
2016-07-18 10:20:25 -0700

    Clean up auth code

--- functions/authenticate/handler.js
+++ functions/authenticate/handler.js
@@ -5,29 +5,26 @@ var jwt = require('jsonwebtoken');
 
 module.exports.handler = function(event, context, cb) {
 
+    // Check username and pass
+    var requestUsername = "";
+    var requestPassword = "";
 
-    var user_id = "123abc";
-    var access_token = "";
+    // Generate jwt access token
     var payload = {
         "iss": "https://api.example.com",
         "scopes": ["admin", "some_thing", "another_thing"],
         "sub": {
-            user_id: user_id
+            user_id: "123abc"
         }
     };
-    access_token = jwt.sign(payload, "process.env.SECRET", {
+    var access_token = jwt.sign(payload, "process.env.SECRET", {
         expiresIn: 60*60*3
     });
 
-
-    console.log("{'Cookie':event['Cookie']}");
+    // Create set-cookie header for return
     var date = new Date();
-    // Get Unix milliseconds at current time plus 365 days
-    date.setTime(+ date + (365 * 86400000)); //24 \* 60 \* 60 \* 100
-    var cookieVal = Math.random().toString(36).substring(7); // Generate a random cookie string
-    var cookieString = "x-api-access-token="+cookieVal+"; expires="+date.toGMTString()+";";
-    //Set-Cookie: mycookie=somevalue; path=/securesite/; Expires=12/12/2010; secure; httpOnly;
+    date.setTime(+ date + (365 * 86400000)); // 24 * 60 * 60 * 100 (365 days)
+    var cookieString = "X-Api-Key="+access_token+"; expires="+date.toGMTString()+";";
 
-
-    return cb(null, {"Cookie": cookieString, "Token":access_token});
+    return cb(null, {"Cookie": cookieString});
 };
