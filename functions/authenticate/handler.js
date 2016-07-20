1cd4520b9d4b769e1e0c56f777844a65eeacef33
Dan Hogue <chmod740@gmail.com>
2016-07-20 09:03:49 -0700

    clean up auth and origionate policy in authenticate func

--- functions/authenticate/handler.js
+++ functions/authenticate/handler.js
@@ -4,26 +4,27 @@ var bcrypt = require('bcryptjs');
 var jwt = require('jsonwebtoken');
 
 module.exports.handler = function(event, context, cb) {
-    // Check username and pass
-    var requestUsername = event.body.username;
-    var requestPassword = event.body.password;
-    // TODO: Replace hardcoded username/password check with call to db or whatever
-    if (requestUsername != "admin" || requestPassword != "password") {
+
+    // Get user info and check username and pass (You'll need to update this for production!)
+    if (event.body.username != "admin" || event.body.password != "password") {
         return cb("Error, incorrect username or password", null);
     }
 
-    // Generate jwt access token
-    // TODO Update example values
+    // Generate jwt access token with user permissions, and sign
     var payload = {
         "iss": "https://api.example.com",
-        "scopes": ["admin", "some_thing", "another_thing"],
-        "sub": {
-            user_id: "123abc"
-        }
+        "sub": "123abcUserIdHere",
+        "scopes": [
+            {
+                Action: 'execute-api:Invoke',
+                Effect: 'Allow',
+                Resource: 'arn:aws:execute-api:us-west-2:280151647364:fsapgttgb6/dev/*'
+            }
+        ]
     };
     // TODO: Update to get secret signing string from env
-    var access_token = jwt.sign(payload, "process.env.SECRET", {
-        expiresIn: 60 * 60 * 12 // 12 hours
+    var access_token = jwt.sign(payload, "process.env.SECRET_TOKEN_SIGNING_STRING", {
+        expiresIn: 60 * 60 * 8 // 8 hours
     });
 
     return cb(null, {"token": access_token});
--- functions/authorize/handler.js
+++ functions/authorize/handler.js
@@ -2,53 +2,21 @@
 var jwt = require('jsonwebtoken');
 
 exports.handler = function(event, context) {
-
-    console.log('Client token: ' + event.authorizationToken);
-    console.log('Method ARN: ' + event.methodArn);
-
     var token = event.authorizationToken;
 
-    // Call oauth provider, crack jwt token, etc.
-    jwt.verify(token, "process.env.SECRET", function(err, decoded) {
+    jwt.verify(token, "process.env.SECRET_TOKEN_SIGNING_STRING", function(err, decoded) {
         if (err) {
-            console.log("error decoding jwt token");
-            console.log(err);
+            context.fail("Unauthorized");
         } else {
-            console.log("decoded jwt token");
-            console.log(decoded);
+            var authResponse = {
+                principalId: decoded.sub,
+                policyDocument: {
+                    Version: '2012-10-17',
+                    Statement: decoded.scopes
+                }
+            };
+            context.succeed(authResponse);
         }
     });
-
-    var result = 'allow';
-
-    switch (result) {
-        case 'allow':
-            context.succeed(generatePolicy('user', 'Allow', 'arn:aws:execute-api:us-west-2:280151647364:fsapgttgb6/dev/*'));
-            break;
-        case 'deny':
-            context.succeed(generatePolicy('user', 'Deny', 'arn:aws:execute-api:us-west-2:280151647364:fsapgttgb6/dev/*'));
-            break;
-        case 'unauthorized':
-            context.fail("Unauthorized");
-            break;
-        default:
-            context.fail("error");
-    }
 };
 
-var generatePolicy = function(principalId, effect, resource) {
-    var authResponse = {
-        'principalId':principalId
-    };
-    if (effect && resource) {
-        authResponse.policyDocument = {
-            'Version': '2012-10-17',
-            'Statement': [{
-                Action: 'execute-api:Invoke',
-                Effect: effect,
-                Resource: resource
-            }]
-        };
-    }
-    return authResponse;
-};
