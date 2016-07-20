a58752188982fd6046a92406c7ccf265c1ab5f0a
Dan Hogue <chmod740@gmail.com>
2016-07-20 09:17:07 -0700

    expect access token header to be in expected format: 'Bearer ahsdjfhksjdhfka...'

--- functions/authorize/handler.js
+++ functions/authorize/handler.js
@@ -2,7 +2,11 @@
 var jwt = require('jsonwebtoken');
 
 exports.handler = function(event, context) {
-    var token = event.authorizationToken;
+    var token = event.authorizationToken.split(" ");
+    if (token[0] !== "Bearer" || !token[1]) {
+        throw "Error: Bearer token not in expected format";
+    }
+    token = token[1];
 
     jwt.verify(token, "process.env.SECRET_TOKEN_SIGNING_STRING", function(err, decoded) {
         if (err) {
